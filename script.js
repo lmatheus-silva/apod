2efbc848513cd79a95475a94921ec1052d194c9d
Matheus Silva <lmatheus.silva@outlook.com>
2026-05-19 10:30:35 -0300

    Requesting APOD

--- script.js
+++ script.js
@@ -4,14 +4,54 @@ const divImagem = document
     .querySelector('.imagem');
 
 async function acessarApi(data) {
-    const json = fetch('https://api.nasa.gov/planetary/apod');
+    const response = 
+        await fetch(
+        'https://api.nasa.gov/planetary/apod?api_key=' + chave + '&date=' + data
+        );
+
+    if (response.status !== 200) {
+        alert('Não consegui acessar a api');
+        return null;
+    }
+
+    const dados = await response.json();
+
+    if (dados.media_type === 'image') {
+        return {
+            title: dados.title,
+            date: dados.date,
+            desc: dados.explanation,
+            img: dados.url
+        }
+    }
+    
+    alert('Não é imagem')
+    return null; 
 }
 
 document
     .querySelector('form')
-    .addEventListener('submit', (event) => {
+    .addEventListener('submit', async (event) => {
         event.preventDefault();
 
         const form = event.target;
         const input = form.querySelector('input');
+
+        const date = input.value;
+        divImagem.style.display = 'none';
+
+        const dados = await acessarApi(date);
+        if (!dados) return;
+
+        const img = divImagem.querySelector('img');
+        const h2 = divImagem.querySelector('h2');
+        const small = divImagem.querySelector('small');
+        const p = divImagem.querySelector('p');
+
+        img.setAttribute('src', dados.img);
+        h2.innerHTML = dados.title;
+        small.innerHTML = dados.date;
+        p.innerHTML = dados.desc;
+
+        divImagem.style.display = 'block';
     });
